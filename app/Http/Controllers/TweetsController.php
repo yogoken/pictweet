3fa8dc9b57bc3e85a4c7ec69aa427f1d0b126ade
yogoken <yogo.k@husky.neu.edu>
2017-01-01 15:22:17 +0900

    Can post name/image/comment

--- app/Http/Controllers/TweetsController.php
+++ app/Http/Controllers/TweetsController.php
@@ -14,4 +14,21 @@ class TweetsController extends Controller
     $tweets = Tweet::all();
     return view('tweets.index')->with('tweets', $tweets);
   }
+
+  public function create()
+  {
+    return view('tweets.create');
+  }
+
+  public function store(Request $request)
+  {
+    Tweet::create(
+      array(
+        'name' => $request->name,
+        'image' => $request->image,
+        'text' => $request->text
+      )
+    );
+    return view('tweets.store');
+  }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,4 +28,6 @@ Route::get('/', function () {
 
 Route::group(['middleware' => ['web']], function () {
   Route::get('/tweets', 'TweetsController@index');
+  Route::get('/tweets/create', 'TweetsController@create');
+  Route::post('/tweets', 'TweetsController@store');
 });
